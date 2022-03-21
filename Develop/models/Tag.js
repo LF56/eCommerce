7022ddea26260775bd2c6474d4a2409f8965b409
LF56 <leah.fox7@gmail.com>
2022-03-20 20:27:54 -0400

    added tags models

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -4,12 +4,14 @@ const sequelize = require('../config/connection.js');
 
 class Tag extends Model {}
 
-//id- integer, no null values, set primary keu, auto incremeent
-
-
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    }
   },
   {
     sequelize,
